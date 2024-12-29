8594e829088bc0679098ac06e753bc6183e8eff0
Asaph Velazquez <saulvelpa120@gmail.com>
2024-12-28 20:58:35 -0600

    actualiacion

--- FoodMart-1.0.0/php/login.php
+++ FoodMart-1.0.0/php/login.php
@@ -1,32 +1,32 @@
 <?php
-$conexion = mysqli_connect('localhost', 'root', '', 'escomdeals');
+    session_start();
+    $conexion = mysqli_connect('localhost', 'root', '', 'escomdeals');
 
-if (!$conexion) {
-    die('Error al conectar con la base de datos: ' . mysqli_connect_error());
-}
-
-$email = $_POST['email'] ?? '';
-$contrasena = $_POST['password'] ?? '';
-
-$consulta = "SELECT * FROM usuario WHERE correo = '$email'";
-$resultado = mysqli_query($conexion, $consulta);
-
-if (!$resultado) {
-    die('Error en la consulta: ' . mysqli_error($conexion));
-}
-
-// Validar si el usuario existe
-if (mysqli_num_rows($resultado) > 0) {
-    $usuario = mysqli_fetch_assoc($resultado);
-    if (password_verify($contrasena, $usuario['contraseña'])) {
-        header("Location: ../index.html");
-        exit();
-    } else {
-        echo 'La contraseña es incorrecta.';
+    if (!$conexion) {
+        die('Error al conectar con la base de datos: ' . mysqli_connect_error());
     }
-} else {
-    echo 'El usuario no existe.';
-}
 
-mysqli_close($conexion);
+    $email = $_POST['email'] ?? '';
+    $contrasena = $_POST['password'] ?? '';
+
+    $consulta = "SELECT * FROM usuario WHERE correo = '$email'";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    if (!$resultado) {
+        die('Error en la consulta: ' . mysqli_error($conexion));
+    }
+
+    // Validar si el usuario existe
+    if (mysqli_num_rows($resultado) > 0) {
+        $usuario = mysqli_fetch_assoc($resultado);
+        if (password_verify($contrasena, $usuario['contraseña'])) {
+            header("Location: ../index.html");
+            exit();
+        } else {
+            echo 'La contraseña es incorrecta.';
+        }
+    } else {
+        echo 'El usuario no existe.';
+    }
+    mysqli_close($conexion);
 ?>
